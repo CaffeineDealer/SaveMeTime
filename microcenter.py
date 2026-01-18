dbf18eeeb8f4ac6991a6fc3f0c6b47927c5e4000
exeKor <yavar.korkian@gmail.com>
2026-01-18 13:04:09 -0500

    init

---
+++ microcenter.py
@@ -0,0 +1,36 @@
+# Objective: Parse inventory information across different stores for any deals in microcenter 
+# Created: Jan 18 2026
+# Last Modified: Jan 18 2026
+# Author: CaffeineDealer
+
+# HTTP, expression matching, JSON
+import requests
+import re
+import json
+
+def check_microcenter_stock(product_id):
+    url = f"https://www.microcenter.com/product/{product_id}/hmm-coffee"
+
+    # get the page
+    response = requests.get(url)
+    html = response.text
+
+    # Find inventory data: search for a pattern
+    match = re.search(r'var inventory = (\[.*?\])', html, re.DOTALL)
+
+    if not match:
+        print("No Inventory Data!")
+        return
+    
+    # Parse JSON
+    inventory = json.loads(match.group(1))
+    print(f"Store Availability {product_id}:\n")
+    inStock = [store for store in inventory if store['qoh'] > 0]
+    if inStock:
+        for store in inStock:
+            print(f"{store['storeName']}: {store['qoh']} in stock")
+    else:
+        print("Out of Stock!") 
+
+product_id = input("Enter product ID")
+check_microcenter_stock(product_id)
